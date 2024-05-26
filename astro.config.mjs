46090c626dc8d732fd14e9f4a4b38b8d9ef43670
tylerguy <tylerdenman200@gmail.com>
2024-05-26 15:55:29 +0100

    remove astro-compress package

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,8 @@
 import { defineConfig } from "astro/config";
 import sitemap from "@astrojs/sitemap";
 
-import compress from "astro-compress";
-
 // https://astro.build/config
 export default defineConfig({
   site: "https://terrashift.net",
-  integrations: [sitemap(), compress()],
+  integrations: [sitemap()],
 });
