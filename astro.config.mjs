8757dab93223255a1a9d61f2f24a2e44e9dbe4f9
tylerguy <tylerdenman200@gmail.com>
2025-01-27 12:50:59 +0000

    switch to tailwind for css

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,10 @@
 import { defineConfig } from "astro/config";
 import sitemap from "@astrojs/sitemap";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://terrashift.net",
   integrations: [sitemap()],
+  vite: { plugins: [tailwindcss()] },
 });
